5ae7be0cd09f425ef0f2c7ec895277f68a876b15
Brian Hartigan <hartiganbg@gmail.com>
2019-05-16 07:19:54 -0400

    Added user queries for get user by name, dob, email, and phonenumber

--- data/users.js
+++ data/users.js
@@ -29,12 +29,69 @@ getUserById(id) {
                 });
             });
           }).catch((error) => {
-        //Is Try Catch necessary here? 
         console.log("ERROR: " + error);
     });
 },
 
-createUser(name) {
+getUserByName(name) {
+    validate.verifyString(name)
+        return users()
+        .then(userCollection => {
+            return userCollection.findOne({ name: name })
+            .then(user => {
+            if (!user) throw "WARN: " + "Could not find user with name " + name;
+            return user;
+            });
+          }).catch((error) => { 
+        console.log("ERROR: " + error);
+    });
+},
+
+getUserByDOB(dob) {
+    validate.verifyString(dob)
+        return users()
+        .then(userCollection => {
+            return userCollection.findOne({ dob: dob })
+            .then(user => {
+            if (!user) throw "WARN: " + "Could not find user with dob " + dob;
+            return user;
+            });
+          }).catch((error) => { 
+        console.log("ERROR: " + error);
+    });
+},
+
+getUserByEmail(email) {
+    //add regex for email
+    validate.verifyString(email)
+        return users()
+        .then(userCollection => {
+            return userCollection.findOne({ email: email })
+            .then(user => {
+            if (!user) throw "WARN: " + "Could not find user with email " + email;
+            return user;
+            });
+          }).catch((error) => { 
+        console.log("ERROR: " + error);
+    });
+},
+
+getUserByPhoneNumber(phoneNumber) {
+    //add regex for phone number
+    validate.verifyString(phoneNumber)
+        return users()
+        .then(userCollection => {
+            return userCollection.findOne({ phoneNumber: phoneNumber })
+            .then(user => {
+            if (!user) throw "WARN: " + "Could not find user with phoneNumber " + phoneNumber;
+            return user;
+            });
+          }).catch((error) => { 
+        console.log("ERROR: " + error);
+    });
+},
+
+createUser(name, dob, email, phoneNumber) {
     return validate.verifyString(name, "name")
         .then(() => {
             return users()
@@ -42,7 +99,10 @@ createUser(name) {
                 let newUser = {
                     //_id: "",
                     name: name,
-                    likes: []
+                    dob: dob,
+                    email: email,
+                    phoneNumber: phoneNumber,
+                    ratedVenues: []
                 }            
                 return userCollection.insertOne(newUser)
                 .then((insertInfo) => {
