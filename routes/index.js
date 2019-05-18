bf4d5db26e7304f5d8927dad5fdfa68be5f1c29b
Alisha Singh <asingh42@stevens.edu>
2019-05-18 12:32:59 -0400

    Solved AJAX bugs, added userid in routes

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,18 @@ const path = require("path");
 const constructorMethod = app => {
   app.use("/users", userRoutes);
 
-  //add userid as req param in venues search and about us while the user is logged in and home 
-  app.use("/venues",venuesRoutes);
-  app.use("/search", searchRoutes);
+  //add userid as req param in venues search and about us while the user is logged in and home2 
+  app.use("/:userid/venues",venuesRoutes);
+  app.use("/:userid/search", searchRoutes);
   
   //add userid as a tag in the below route
-  app.get("/home", async (req, res) => {
+  app.get("/:userid/home", async (req, res) => {
     res.render("pages/home", {title:"Home"})
 })
-  app.get("/test", (req,res) => { 
-    let route = path.resolve("static/testLandingPage.html");
-    res.sendFile(route);
-  });
+  // app.get("/test", (req,res) => { 
+  //   let route = path.resolve("static/testLandingPage.html");
+  //   res.sendFile(route);
+  // });
 
   // app.use("*", (req, res) => {
   //   res.render("pages/aboutUs");
